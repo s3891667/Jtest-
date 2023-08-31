29db2eba6ffafc0b617ce21fe24cea2466edaca6
s3891667 <s3891667@rmit.edu.vn>
2023-08-31 23:19:05 +1000

    config testing and add nested test cases

--- src/main/java/org/example/Event.java
+++ src/main/java/org/example/Event.java
@@ -7,225 +7,201 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Event {
-    static ArrayList<BasicData> Admin;
-    static ArrayList<BasicData> Student;
+	static ArrayList<BasicData> Admin;
+	static ArrayList<BasicData> Student;
 
-    Event()
-    {
-        try{
-            Admin = new ArrayList<BasicData>();
-            Student = new ArrayList<BasicData>();
+	public ArrayList<BasicData> getAdmin() {
+		return Admin;
+	}
 
-            BufferedReader in_Admin = new BufferedReader (new FileReader("admin.txt"));
-            BufferedReader in_Student = new BufferedReader (new FileReader("student.txt"));
+	public ArrayList<BasicData> getStudent() {
+		return Student;
+	}
 
-            String s;
-            String id;
-            String name;
-            String password;
+	Event() {
+		try {
+			Admin = new ArrayList<BasicData>();
+			Student = new ArrayList<BasicData>();
 
-            while((s = in_Admin.readLine()) != null)
-            {
-                id = s.substring(3,s.indexOf("Name") - 2);
-                name = s.substring(s.indexOf("Name") + 5, s.indexOf("Password") - 2);
-                password = s.substring(s.indexOf("Password") + 9, s.length());
+			BufferedReader in_Admin = new BufferedReader(new FileReader("admin.txt"));
+			BufferedReader in_Student = new BufferedReader(new FileReader("student.txt"));
 
-                BasicData obj = new BasicData(Integer.parseInt(id), name, password);
-                Admin.add(obj);
+			String s;
+			String id;
+			String name;
+			String password;
 
-            }
+			while ((s = in_Admin.readLine()) != null) {
+				id = s.substring(3, s.indexOf("Name") - 2);
+				name = s.substring(s.indexOf("Name") + 5, s.indexOf("Password") - 2);
+				password = s.substring(s.indexOf("Password") + 9, s.length());
 
-            while((s = in_Student.readLine()) != null)
-            {
-                id = s.substring(3,s.indexOf("Name") - 2);
-                name = s.substring(s.indexOf("Name") + 5, s.indexOf("Password") - 2);
-                password = s.substring(s.indexOf("Password") + 9, s.length());
+				BasicData obj = new BasicData(Integer.parseInt(id), name, password);
+				Admin.add(obj);
 
-                BasicData obj = new BasicData(Integer.parseInt(id), name, password);
-                Student.add(obj);
+			}
 
-            }
+			while ((s = in_Student.readLine()) != null) {
+				id = s.substring(3, s.indexOf("Name") - 2);
+				name = s.substring(s.indexOf("Name") + 5, s.indexOf("Password") - 2);
+				password = s.substring(s.indexOf("Password") + 9, s.length());
 
-            in_Admin.close();
-            in_Student.close();
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
-    }
+				BasicData obj = new BasicData(Integer.parseInt(id), name, password);
+				Student.add(obj);
 
+			}
 
-    public boolean AdminLogin()
-    {
-        Scanner in = new Scanner(System.in);
-        String name;
-        String password;
+			in_Admin.close();
+			in_Student.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-        System.out.print("\nEnter Admin's Name: ");
-        name = in.nextLine();
-        System.out.print("Enter Admin's Password: ");
-        password = in.nextLine();
+	public boolean AdminLogin() {
+		Scanner in = new Scanner(System.in);
+		String name;
+		String password;
 
-        for(int i = 0; i<Admin.size();i++)
-        {
-            if(Admin.get(i).getName().equals(name) && Admin.get(i).getPassword().equals(password))
-                return true;
-        }
-        return false;
-    }
+		System.out.print("\nEnter Admin's Name: ");
+		name = in.nextLine();
+		System.out.print("Enter Admin's Password: ");
+		password = in.nextLine();
 
+		for (int i = 0; i < Admin.size(); i++) {
+			if (Admin.get(i).getName().equals(name) && Admin.get(i).getPassword().equals(password))
+				return true;
+		}
+		return false;
+	}
 
-    public boolean StudentLogin()
-    {
-        Scanner in = new Scanner(System.in);
-        int sID;
-        String password;
+	public boolean StudentLogin() {
+		Scanner in = new Scanner(System.in);
+		int sID;
+		String password;
 
-        System.out.print("\nEnter Student's ID: ");
-        sID = in.nextInt();
-        in.nextLine();
-        System.out.print("Enter Student's Password: ");
-        password = in.next();
+		System.out.print("\nEnter Student's ID: ");
+		sID = in.nextInt();
+		in.nextLine();
+		System.out.print("Enter Student's Password: ");
+		password = in.next();
 
-        for(int i = 0; i<Student.size();i++)
-        {
-            if(Student.get(i).getID() == sID && Student.get(i).getPassword().equals(password))
-                return true;
-        }
-        return false;
-    }
+		for (int i = 0; i < Student.size(); i++) {
+			if (Student.get(i).getID() == sID && Student.get(i).getPassword().equals(password))
+				return true;
+		}
+		return false;
+	}
 
-    public void showStudentEvents()
-    {
-        try{
-            BufferedReader in = new BufferedReader (new FileReader("event.txt"));
-            String s;
-            System.out.println("\nList of Events: ");
+	public void showStudentEvents() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("event.txt"));
+			String s;
+			System.out.println("\nList of Events: ");
 
-            while ((s = in.readLine()) != null)
-                System.out.println("o "  + s);
-        }
-        catch(Exception e)
-        { }
-    }
+			while ((s = in.readLine()) != null)
+				System.out.println("o " + s);
+		} catch (Exception e) {
+		}
+	}
 
+	public boolean viewStudentDetails() {
+		System.out.print("\nDetails of All Student:.");
+		for (int i = 0; i < Student.size(); i++)
+			Student.get(i).print();
+		if (Student.size() > 0)
+			return true;
+		return false;
+	}
 
-    public boolean viewStudentDetails()
-    {
-        System.out.print("\nDetails of All Student:.");
-        for(int i = 0; i<Student.size();i++)
-            Student.get(i).print();
-        if(Student.size() > 0)
-            return true;
-        return false;
-    }
+	public boolean searchStudentDetails(int id) {
+		for (int i = 0; i < Student.size(); i++) {
+			if (Student.get(i).getID() == id) {
+				Student.get(i).print();
+				return true;
+			}
+		}
+		return false;
 
-    public boolean searchStudentDetails(int id)
-    {
-        for(int i = 0; i<Student.size();i++)
-        {
-            if(Student.get(i).getID() == id)
-            {
-                Student.get(i).print();
-                return true;
-            }
-        }
-        return false;
+	}
 
-    }
+	public boolean removeStudent(int id) {
 
-    public boolean removeStudent(int id)
-    {
+		for (int i = 0; i < Student.size(); i++) {
+			if (Student.get(i).getID() == id) {
+				Student.remove(i);
+				rewriteStudentFile();
+				return true;
+			}
+		}
+		return false;
+	}
 
-        for(int i = 0; i<Student.size();i++)
-        {
-            if(Student.get(i).getID() == id)
-            {
-                Student.remove(i);
-                rewriteStudentFile();
-                return true;
-            }
-        }
-        return false;
-    }
+	public int countStudent() {
+		return Student.size();
+	}
 
-    public int countStudent()
-    {
-        return Student.size();
-    }
+	public static void rewriteStudentFile() {
+		try {
+			BufferedWriter out_Student = new BufferedWriter(new FileWriter("student.txt"));
 
-    public static void rewriteStudentFile()
-    {
-        try{
-            BufferedWriter out_Student = new BufferedWriter (new FileWriter("student.txt"));
+			for (int i = 0; i < Student.size(); i++) {
+				out_Student.write("Id:" + Student.get(i).getID() + ", ");
+				out_Student.write("Name:" + Student.get(i).getName() + ", ");
+				out_Student.write("Password:" + Student.get(i).getPassword());
+				out_Student.write("\n");
+			}
 
-            for(int i = 0; i < Student.size();i++)
-            {
-                out_Student.write("Id:" + Student.get(i).getID() + ", ");
-                out_Student.write("Name:" + Student.get(i).getName() + ", ");
-                out_Student.write("Password:" + Student.get(i).getPassword());
-                out_Student.write("\n");
-            }
+			out_Student.close();
 
-            out_Student.close();
+		} catch (Exception e) {
+		}
+	}
 
-        }
-        catch(Exception e)
-        { }
-    }
+	public String AddStudent() {
 
+		try {
 
-    public String AddStudent(){
+			Scanner in = new Scanner(System.in);
+			int cID;
+			String cname;
+			String cpassword;
 
-        try {
+			System.out.print("Enter Student's ID: ");
+			cID = in.nextInt();
+			in.nextLine();
 
-            Scanner in = new Scanner(System.in);
-            int cID;
-            String cname;
-            String cpassword;
+			if (!searchStudentDetails(cID)) {
 
-            System.out.print("Enter Student's ID: ");
-            cID = in.nextInt();
-            in.nextLine();
+				System.out.print("Enter Student's Name: ");
+				cname = in.nextLine();
+				System.out.print("Enter Student's Password: ");
+				cpassword = in.nextLine();
 
-            if (!searchStudentDetails(cID)) {
+				ValidatePassword(cpassword);
 
-                System.out.print("Enter Student's Name: ");
-                cname = in.nextLine();
-                System.out.print("Enter Student's Password: ");
-                cpassword = in.nextLine();
+				BasicData obj = new BasicData(cID, cname, cpassword);
+				Student.add(obj);
+				rewriteStudentFile();
+				return "Student Added Successfully";
+			} else {
+				return "Student Exists";
+			}
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
 
-                ValidatePassword(cpassword);
+	private static void ValidatePassword(@NotNull String password) throws PasswordValidationException {
+		if (password.length() != 9) {
+			throw new PasswordValidationException("Password length should be 9", new RuntimeException());
+		} else if (!"p".equals(password.substring(0, 1))) {
+			throw new PasswordValidationException("First letter of the Password should be p",
+					new RuntimeException());
+		} else if (!"#".equals(password.substring(8, 9))) {
+			throw new PasswordValidationException("Last letter of the password should be #",
+					new RuntimeException());
+		}
+	}
 
-                BasicData obj = new BasicData(cID, cname, cpassword);
-                Student.add(obj);
-                rewriteStudentFile();
-                return "Student Added Successfully";
-            }
-            else
-            {
-                return "Student Exists";
-            }
-        }
-        catch (Exception e)
-        {
-            return e.getMessage();
-        }
-    }
-
-    private static void ValidatePassword(@NotNull String password) throws PasswordValidationException {
-        if (password.length() != 9)
-        {
-            throw new PasswordValidationException("Password length should be 9", new RuntimeException());
-        }
-        else  if (!"p".equals(password.substring(0,1)))
-        {
-            throw new PasswordValidationException("First letter of the Password should be p", new RuntimeException());
-        }
-        else  if (!"#".equals(password.substring(8,9)))
-        {
-            throw new PasswordValidationException("Last letter of the password should be #", new RuntimeException());
-        }
-    }
 }
--- src/test/java/org/example/a2test.java
+++ src/test/java/org/example/a2test.java
@@ -1,13 +1,101 @@
 package org.example;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class a2test {
-     @Test
-     public void evaluatesExpression() {
-        Calculator calculator = new Calculator();
-        int sum = calculator.evaluate("1+2+3");
-        assertEquals(6, sum);
-     }
+	@Test
+	public void testInput() {
+		Event event = new Event();
+		Integer admin = event.getAdmin().size();
+		Integer student = event.getStudent().size();
+		Assertions.assertTrue(admin > 0);
+		Assertions.assertTrue(student > 0);
+	}
+
+	@Tag("testSet1")
+	@Nested
+	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+	class TestingInputFeature {
+
+		@Test
+		public void testAdminLogin() {
+			Event event = new Event();
+			String input = "owen\npassword\n";
+			System.setIn(new ByteArrayInputStream(input.getBytes()));
+			Boolean result = event.AdminLogin();
+			Assertions.assertFalse(result);
+
+			System.setIn(System.in);
+		}
+
+		@Test
+		public void testStudentLogin() {
+			Event event = new Event();
+			String input = "7654324\np7654324#\n";
+			System.setIn(new ByteArrayInputStream(input.getBytes()));
+			Boolean result = event.StudentLogin();
+			Assertions.assertTrue(result);
+
+			System.setIn(System.in);
+
+		}
+	}
+
+	@Tag("testSet2")
+	@Nested
+	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+	class CRUDTest {
+		@Test
+		public void testShowStudentEvents() {
+			Event event = new Event();
+
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			PrintStream originalOut = System.out; // Store original System.out
+
+			System.setOut(new PrintStream(outputStream));
+
+			event.showStudentEvents();
+
+			System.setOut(originalOut);
+
+			String capturedOutput = outputStream.toString();
+
+			Assertions.assertEquals("", capturedOutput);
+
+		}
+
+		@Test
+		public void testviewStudentDetails() {
+			Event event = new Event();
+			Assertions.assertTrue(event.viewStudentDetails());
+		}
+
+		@Test
+		public void testSearchStudentDetails() {
+			Integer id = 1234;
+			Event event = new Event();
+			Assertions.assertTrue(event.searchStudentDetails(id));
+		}
+
+		@Test
+		public void testRemoveStudent() {
+			Event event = new Event();
+			Integer id = 7654324;
+			Assertions.assertAll(() -> assertTrue(event.removeStudent(id)),
+					() -> assertFalse(event.searchStudentDetails(id)));
+		}
+
+	}
+
 }
